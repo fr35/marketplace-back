79a55ea1da755c14c28511da000b36891eedf8c4
Facundo Rodriguez <facundorodriguezgranieri@gmail.com>
2023-01-31 22:40:01 -0300

    push

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ app.get('*', (req, res) => {
     res.send(`Ruta ${method} ${url} no está implementada`)
 })
 
-app.get('/', HomeRouter)
+app.use('/', HomeRouter)
 app.use('/auth', UserRouter)
 app.use("/products", ProductRouter)
 app.use("/cart", CartRouter)
