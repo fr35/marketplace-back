a875120a1e22aee6fcb48b133dc5e6723e9e6fe3
Facundo Rodriguez <facundorodriguezgranieri@gmail.com>
2023-01-31 22:37:38 -0300

    push

--- src/routers/home.js
+++ src/routers/home.js
@@ -1,10 +1,9 @@
 import { Router } from "express";
-import { ProductController } from "../controllers/product.js";
+
 
 
 const router = Router()
 
-// Se verifica que el usuario tenga el token del log in activo
 router.get("/", (req,res) => {
     res.send('servidor en Home')
 })
